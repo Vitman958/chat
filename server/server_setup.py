ed5a157290f177647dba3270a6269dbdca787fd6
Vitman <vitman_958@hotmail.com>
2025-09-29 01:25:59 +0300

    refc-arch

---
+++ server/server_setup.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from utils.logger_setup import get_logger
+
+
+logger = get_logger(__name__)
+
+
+def create_handler(users, server_name, handle_read, handle_write):
+    async def handle_client(reader, writer):
+        name = await reader.readline()
+        nick_name = name.decode().strip()
+
+        if users.check_user(nick_name):  
+            users.add_user(writer, nick_name)
+        else:
+            writer.write(f"Этот никней уже занят\n".encode())
+            await writer.drain()
+            writer.close()
+            return 
+
+        print(f"Пользователь [{nick_name}] подключился на сервер")
+        logger.info(f"Пользователь {nick_name} подключился к серверу")
+
+        stop_event = asyncio.Event()
+        read_task = asyncio.create_task(
+            handle_read(reader, writer, nick_name, stop_event, users)
+        )
+        write_task = asyncio.create_task(
+            handle_write(writer, server_name, stop_event, users)
+        )
+
+        await stop_event.wait()
+        await read_task
+        await write_task
+        writer.close()
+        await writer.wait_closed()
+    return handle_client
