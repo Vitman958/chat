da85acda1c17e5c8cc43177b1ccf87e3fe88cba5
Vitman <vitman_958@hotmail.com>
2025-10-09 23:13:39 +0300

    удален тест на работу рассылки сообщений

--- shared/broadcast.py
+++ shared/broadcast.py
@@ -5,7 +5,6 @@ from datetime import datetime
 async def broadcast(message, users, sender_name, exclude_writer=None):
     writers = users.get_users()
     time = datetime.now().strftime("%H:%M")
-    print(f"Broadcasting to {len(writers)} users")
     for writer in writers:
         if writer is exclude_writer:
             continue
