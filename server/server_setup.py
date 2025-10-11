adacc7f0ccb3fd383146e6e9a7c8c7fd6c101a3e
Vitman <vitman_958@hotmail.com>
2025-10-11 22:56:13 +0300

    Реализована рассылка о подключении пользователя

--- server/server_setup.py
+++ server/server_setup.py
@@ -6,7 +6,7 @@ from utils.logger_setup import get_logger
 logger = get_logger(__name__)
 
 
-def create_handler(users, server_name, handle_read, handle_write):
+def create_handler(users, server_name, handle_read, handle_write, broadcast):
     async def handle_client(reader, writer):
         name = await reader.readline()
         nick_name = name.decode().strip()
@@ -19,6 +19,8 @@ def create_handler(users, server_name, handle_read, handle_write):
             writer.close()
             return 
 
+        connection_msg = f"Пользователь [{nick_name}] подключился к серверу"
+        await broadcast(connection_msg, users, server_name, exclude_writer = writer)
         print(f"Пользователь [{nick_name}] подключился на сервер")
         logger.info(f"Пользователь {nick_name} подключился к серверу")
 
