af55a5e63f77fea449d17b712da0ca61bdf07512
Vitman <vitman_958@hotmail.com>
2025-10-26 13:21:18 +0300

    Интеграция Room в серверную часть

--- server/server_setup.py
+++ server/server_setup.py
@@ -6,30 +6,36 @@ from utils.logger_setup import get_logger
 logger = get_logger(__name__)
 
 
-def create_handler(users, server_name, handle_read, handle_write, broadcast):
+def create_handler(users, server_name, handle_read, handle_write, broadcast, room_manager):
     async def handle_client(reader, writer):
         name = await reader.readline()
         nick_name = name.decode().strip()
 
+        default_room = room_manager.get_room("general")
+    
         if users.check_user(nick_name):  
             users.add_user(writer, nick_name)
+            room_manager.assign_user_to_room(writer, default_room)
         else:
             writer.write(f"Этот никней уже занят\n".encode())
             await writer.drain()
             writer.close()
-            return 
-
+            return
+        
         connection_msg = f"Пользователь [{nick_name}] подключился к серверу"
         await broadcast(connection_msg, users, server_name, exclude_writer = writer)
         print(f"Пользователь [{nick_name}] подключился на сервер")
         logger.info(f"Пользователь {nick_name} подключился к серверу")
 
         stop_event = asyncio.Event()
+        
+        user_room = room_manager.get_user_room(writer)
+
         read_task = asyncio.create_task(
-            handle_read(reader, writer, nick_name, stop_event, users)
+            handle_read(reader, writer, nick_name, stop_event, user_room)
         )
         write_task = asyncio.create_task(
-            handle_write(writer, server_name, stop_event, users)
+            handle_write(writer, server_name, stop_event, user_room)
         )
 
         await stop_event.wait()
