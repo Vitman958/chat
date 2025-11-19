2c2747737d1e642eecdda10f578586f2f602799b
Vitman <vitman_958@hotmail.com>
2025-11-19 19:43:24 +0300

    Реализован модуль проверки имени

---
+++ utils/check_nickname.py
@@ -0,0 +1,16 @@
+def check_nickname(nick_name):
+    has_letter = nick_name.isdigit()
+
+    all_valid = all(char.isalpha() or char.isdigit() or char in ['_', '-'] for char in nick_name)
+
+    if has_letter:
+        return False, f"Никнейм не может состоять только из цифр"
+    
+    if not all_valid:
+        return False, f"Недопустимые символы"
+    
+    if len(nick_name) < 3 or len(nick_name) > 16:
+        return False, f"Превышена длина никнейма"
+
+    return True, ""
+    
