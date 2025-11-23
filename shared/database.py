586917469e624390f8a5fe6ac6732031a9224fb0
Vitman <vitman_958@hotmail.com>
2025-11-23 17:26:31 +0300

    Реализован метод get_messages

--- shared/database.py
+++ shared/database.py
@@ -7,9 +7,10 @@ class DatabaseManager:
 
     async def init_db(self):
         async with aiosqlite.connect(self.db_path) as con:
+            cursor = con.cursor()
 
-            await con.execute("""
-            CREATE TABLE IF NOT EXISTS messages(
+            await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Messages(
                 id INTEGER PRIMARY KEY,
                 room_name TEXT NOT NULL,
                 sender TEXT NOT NULL,
@@ -22,10 +23,26 @@ class DatabaseManager:
 
     async def save_message(self, room_name, sender, message, timestamp):
         async with aiosqlite.connect(self.db_path) as con:
+            cursor = con.cursor()
 
-            await con.execute("""
-            INSERT INTO messages (room_name, sender, message, timestamp)
+            await cursor.execute("""
+            INSERT INTO Messages (room_name, sender, message, timestamp)
             VALUES (?, ?, ?, ?)
             """, (room_name, sender, message, timestamp))
 
             await con.commit()
+
+    async def get_messages(self, room_name, limit=100):
+        async with aiosqlite.connect(self.db_path) as con:
+            cursor = con.cursor()
+
+            await cursor.execute("""
+            SELECT timestamp, sender, message FROM Messages 
+            WHERE room_name = ?
+            ORDER BY id DESC
+            LIMIT ?
+            """, (room_name, limit))
+
+
+            messages = await cursor.fetchall()
+            return messages
