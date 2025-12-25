80f90d5fa1d28af50bed68259cc6f63b0d4509df
Vitman <vitman_958@hotmail.com>
2025-12-25 22:53:03 +0700

    Сохранение пользователей в БД при подключении и отправка истории сообщений

--- server/server_setup.py
+++ server/server_setup.py
@@ -1,4 +1,5 @@
 import asyncio
+import uuid
 
 from utils.list_commands import commands
 from utils.logger_setup import get_logger
@@ -8,8 +9,9 @@ from utils.check_nickname import check_nickname
 logger = get_logger(__name__)
 
 
-def create_handler(users, server_name, handle_read, room_manager, rate_limiter, command_handler, database_manager, auth_manager):
+def create_handler(server_name, handle_read, room_manager, rate_limiter, command_handler, database_manager, auth_manager):
     async def handle_client(reader, writer):
+        """Создание обработчиков подключений"""
         try:
             name = await reader.readline()
             nick_name = name.decode().strip()
@@ -21,18 +23,23 @@ def create_handler(users, server_name, handle_read, room_manager, rate_limiter, 
                 writer.close()
                 return
 
-            default_room = room_manager.get_room("general")
+            default_room = await room_manager.get_room("general")
 
-            if users.check_user(nick_name):  
-                users.add_user(writer, nick_name)
-                default_room.add_users(writer, nick_name)
-                room_manager.assign_user_to_room(writer, default_room)
-
+            if not await database_manager.check_user_exists(nick_name):
+                user_id = str(uuid.uuid4())
+                await room_manager.assign_user_to_room(writer, user_id, "general", nick_name)
+            
             else:
                 writer.write(f"❌ Этот никней уже занят\n".encode())
                 await writer.drain()
                 writer.close()
                 return
+
+            messages = await database_manager.get_messages(room_name="general")
+            for timestamp, sender, message in messages:
+                formatted_msg = f"[{timestamp}][{sender}]: {message}\n"
+                writer.write(formatted_msg.encode())
+                await writer.drain()
 
             connection_msg = f"Пользователь [{nick_name}] подключился к серверу"
 
@@ -40,13 +47,7 @@ def create_handler(users, server_name, handle_read, room_manager, rate_limiter, 
               "\n".join([f"  • {cmd}" for cmd in commands]) + "\n"
 
             writer.write(welcome_msg.encode())
-            await writer.drain()
-
-            messages = await database_manager.get_messages(room_name="general")
-            for timestamp, sender, message in messages:
-                formatted_msg = f"[{timestamp}][{sender}]: {message}\n"
-                writer.write(formatted_msg.encode())
-                await writer.drain()
+            await writer.drain()            
 
             await default_room.send_message(connection_msg, server_name, exclude_writer = writer)
             print(f"Пользователь [{nick_name}] подключился на сервер")
@@ -55,7 +56,7 @@ def create_handler(users, server_name, handle_read, room_manager, rate_limiter, 
             stop_event = asyncio.Event()
 
             read_task = asyncio.create_task(
-                handle_read(reader, writer, nick_name, stop_event, users, room_manager, rate_limiter, command_handler, database_manager, auth_manager)
+                handle_read(reader, writer, nick_name, stop_event, user_id, room_manager, rate_limiter, command_handler, database_manager, auth_manager)
             )
         
             await stop_event.wait()
