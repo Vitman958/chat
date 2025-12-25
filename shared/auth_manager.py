b0b921393067bafc6d6bb6718dcefcc98c800f0d
Vitman <vitman_958@hotmail.com>
2025-12-26 00:04:23 +0700

    Добавлено описание каждой функции

--- shared/auth_manager.py
+++ shared/auth_manager.py
@@ -1,10 +1,12 @@
 class AuthManager:
     def __init__(self, database_manager):
+        """Инициализация Менеджера БД"""
         self.database_manager = database_manager
         self.auth_users = {}
         self.auth_writers = set()
 
     async def authenticate(self, writer, nick_name, password):
+        """Аутентификация пользователя"""
         if await self.database_manager.verify_user(nick_name, password):
             self.auth_users[writer] = nick_name
             self.auth_writers.add(writer)
@@ -13,14 +15,17 @@ class AuthManager:
             return False
 
     def is_authenticated(self, writer):
+        """Проверка на аутентификацию пользователя"""
         return writer in self.auth_users
         
     def logout(self, writer):
+        """Выход из системы"""
         if writer in self.auth_users:
             del self.auth_users[writer]
             self.auth_writers.remove(writer)
 
     async def register(self, nick_name, password):
+        """Регистрация пользователя"""
         try:
             await self.database_manager.save_user(nick_name, password)
             return True
