37cceccb60e27fc4c304d8441e3b2aefbef0ba39
Vitman <vitman_958@hotmail.com>
2025-12-26 00:06:30 +0700

    Интеграция БД в RoomManager

--- shared/rooms.py
+++ shared/rooms.py
@@ -1,26 +1,17 @@
 from datetime import datetime
 
-from shared.users import ListClients
-
 
 class Room:
-    def __init__(self, room_name):
+    def __init__(self, room_name, room_manager):
         self.room = room_name
-        self.users = ListClients()
-
-    def add_users(self, writer, nick_name):
-        self.users.add_user(writer, nick_name)
-
-    def remove_users(self, writer):
-        self.users.remove_user(writer)
-
-    def get_users(self):
-        return self.users.get_users()
+        self.room_manager = room_manager
 
     async def send_message(self, message, sender_name, exclude_writer=None):
-        writers = self.users.get_users()
+        users_in_room = await self.room_manager.get_users_in_room(self.room)
+
         time = datetime.now().strftime("%H:%M")
-        for writer in writers:
+
+        for writer, nickname in users_in_room:
             if writer is exclude_writer:
                 continue
             writer.write(f"[{time}][{sender_name}]: {message}\n".encode())
@@ -28,33 +19,49 @@ class Room:
 
 
 class RoomManager:
-    def __init__(self):
-        self.rooms = {}
-        self.user_rooms = {}
+    def __init__(self, database_manager):
+        self.database_manager = database_manager
+        self.user_connections = {}
 
-    def assign_user_to_room(self, writer, room):
-        self.user_rooms[writer] = room
+    async def assign_user_to_room(self, writer, user_id, room_name, nick_name):
+        await self.database_manager.add_user_to_room(room_name, user_id, nick_name)
+        self.user_connections[user_id] = writer
 
-    def get_user_room(self, writer):
-        return self.user_rooms.get(writer)
+    def get_user_connection(self, user_id):
+        return self.user_connections.get(user_id)
+
+    async def get_user_room(self, user_id):
+        room_name = await self.database_manager.get_user_room(user_id)
+        return room_name
     
-    def delete_user_from_rooms(self, writer):
-        if writer in self.user_rooms:
-            del self.user_rooms[writer]
+    async def get_users_in_room(self, room_name):
+        users_data = await self.database_manager.get_users_in_room(room_name)
+        writers = []
+        for user_id, nickname in users_data:
+            writer = self.get_user_connection(user_id)
+            if writer:
+                writers.append((writer, nickname))
+        return writers
+    
+    async def delete_user_from_rooms(self, user_id):
+        await self.database_manager.remove_user_from_room(user_id)
+        if user_id in self.user_connections:
+            del self.user_connections[user_id]
 
-    def create_room(self, room_name):
-        self.rooms[room_name] = Room(room_name)
+    async def create_room(self, room_name):
+        return await self.database_manager.create_room(room_name)
 
-    def check_room(self, room_name):
-        return room_name in self.rooms
+    async def check_room(self, room_name):
+        return await self.database_manager.check_room(room_name)
 
-    def get_room(self, room_name):
-        if self.check_room(room_name):
-            return self.rooms[room_name]
+    async def get_room(self, room_name):
+        room_data = await self.database_manager.get_room(room_name)
+        if room_data:
+            return Room(room_name, self)
         return None
 
-    def get_rooms(self):
-        return self.rooms
+    async def get_rooms(self):
+        return await self.database_manager.get_rooms()
 
-    def delete_room(self, room_name):
-        del self.rooms[room_name]
+    async def delete_room(self, room_name):
+        await self.database_manager.delete_room(room_name)
